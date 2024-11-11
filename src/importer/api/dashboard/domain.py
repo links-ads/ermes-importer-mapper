fc35e2a66efabfe32ebac3b9a2ec28da0efe8da9
Federico Oldani <federico.oldani@gmail.com>
2024-11-11 16:46:50 +0000

    fix removing round to bbox function

--- src/importer/api/dashboard/domain.py
+++ src/importer/api/dashboard/domain.py
@@ -72,5 +72,5 @@ def get_resources(
 
 def get_bbox_from_point(point: str):
     x, y = point.x, point.y
-    bbox = f"{x},{y},{round(x+.000001, 3)},{round(y+.000001, 3)}"
+    bbox = f"{x},{y},{x+.000001},{y+.000001}"
     return bbox
